2a0bd8db4a9a5420a7645e4f5339cfe404bba3e8
zoukari <zoukari20@gmail.com>
2022-10-07 10:41:27 +0200

    changement

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,7 +5,7 @@
 // On a besoin d'Express
 const express = require('express');
 
-// On crée un router avec la méthode mise à disposition par Express
+// On crée un route avec la méthode mise à disposition par Express
 const router = express.Router();
 
 // On associe les fonctions aux différentes routes, on importe le controller
